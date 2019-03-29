587a9a3f129aa06511a3f7e83f1b5415ac3ad892
Bill Riehl <wjriehl@lbl.gov>
2019-03-29 16:54:48 -0700

    some early early prototype plugins

---
+++ kbase-cell-proto/README.md
@@ -0,0 +1,32 @@
+# kbase-cell-proto
+
+A JupyterLab extension.
+
+
+## Prerequisites
+
+* JupyterLab
+
+## Installation
+
+```bash
+jupyter labextension install kbase-cell-proto
+```
+
+## Development
+
+For a development install (requires npm version 4 or later), do the following in the repository directory:
+
+```bash
+npm install
+npm run build
+jupyter labextension link .
+```
+
+To rebuild the package and the JupyterLab app:
+
+```bash
+npm run build
+jupyter lab build
+```
+
---
+++ kbase-cell-proto/src/index.ts
@@ -0,0 +1,58 @@
+import {
+  JupyterLab, JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import {
+  IEditorServices
+} from '@jupyterlab/codeeditor';
+
+import {
+  StaticNotebook, NotebookPanel
+} from '@jupyterlab/notebook';
+
+import {
+  CodeCell, Cell
+} from '@jupyterlab/cells';
+
+import '../style/index.css';
+
+const factory: JupyterLabPlugin<NotebookPanel.IContentFactory> = {
+  id: '@kbase/narrative-extension:factory',
+  provides: NotebookPanel.IContentFactory,
+  requires: [IEditorServices],
+  autoStart: true,
+  activate: (app: JupyterLab, editorServices: IEditorServices) => {
+    let editorFactory = editorServices.factoryService.newInlineEditor;
+    return new KBaseContentFactory({ editorFactory });
+  }
+};
+
+export class KBaseContentFactory extends NotebookPanel.ContentFactory {
+  constructor(options?: Cell.ContentFactory.IOptions) {
+    super(options);
+  }
+
+  createCodeCell(options: CodeCell.IOptions, parent: StaticNotebook): CodeCell {
+    if (!options.contentFactory) {
+      options.contentFactory = this;
+    }
+    // can respond to metadata here.
+
+    // make a new content factory for App Cells?
+    // use it here for the Code Cell.
+    let cell = new CodeCell(options);
+
+    return cell;
+  }
+
+  createAppCell(options: CodeCell.IOptions, parent: StaticNotebook): CodeCell {
+    if (!options.contentFactory) {
+      options.contentFactory = this;
+    }
+
+    let cell = new CodeCell(options);
+    return cell;
+  }
+};
+
+export default factory;
