e813e14489faefd370c06d44166d292569e32528
Bill Riehl <wjriehl@lbl.gov>
2019-06-10 15:45:05 -0700

    some tweaking and updates

--- kbase-cell-proto/src/index.ts
+++ kbase-cell-proto/src/index.ts
@@ -15,6 +15,35 @@ import {
 } from '@jupyterlab/cells';
 
 import '../style/index.css';
+import { IObservableMap } from '@jupyterlab/observables';
+import { JSONValue } from '@phosphor/coreutils';
+import { Widget, PanelLayout } from '@phosphor/widgets';
+
+
+export class KBaseWidget extends Widget {
+  readonly metadata: IObservableMap<JSONValue>;
+
+  constructor(metadata: IObservableMap<JSONValue>) {
+    super();
+    this.metadata = metadata;
+    this.addClass('kb-cell');
+    let str = 'I am a KBase cell. BEHOLD MY GLORY!';
+    let div = document.createElement('div');
+    div.innerText = str;
+    this.node.appendChild(div);
+  }
+}
+
+function updateForKBase(cell: Cell): void {
+  console.log('new KBase cell!');
+  console.log(cell);
+  let kbWidget = new KBaseWidget(cell.model.metadata);
+  const layout = cell.layout as PanelLayout;
+  layout.insertWidget(2, kbWidget);
+  // (cell.inputArea.layout as BoxLayout).addWidget(kbWidget);
+  cell.update();
+}
+
 
 const factory: JupyterLabPlugin<NotebookPanel.IContentFactory> = {
   id: '@kbase/narrative-extension:factory',
@@ -38,23 +67,41 @@ export class KBaseContentFactory extends NotebookPanel.ContentFactory {
     }
     // can respond to metadata here.
 
+    // try this flow.
+    // 1. createCodeCell should inject a Kbase DOM node in all cells.
+    // 2. adding metadata and triggering a re-render should be captured here
+    // or some other function should be triggerable to re-render
+    // 3. Re-rendering a cell should look at metadata and do stuff with the KBase DOM node.
+    // -- make it KBase-ified. Maybe also look at how ipywidgets works?
+
+
     // make a new content factory for App Cells?
     // use it here for the Code Cell.
     let cell = new CodeCell(options);
-    console.log('LET THERE BE A CODE CELL!');
-
-    return cell;
-  }
-
-  createAppCell(options: CodeCell.IOptions, parent: StaticNotebook): CodeCell {
-    if (!options.contentFactory) {
-      options.contentFactory = this;
+    cell.model.metadata.changed.connect(
+      this._metadataChangedHandler,
+      cell
+    )
+    if (cell.model.metadata.keys().indexOf('kbase') != -1) {
+      updateForKBase(cell);
     }
-
-    let cell = new CodeCell(options);
-    alert('LET THERE BE AN APP CELL!');
     return cell;
   }
+
+  _metadataChangedHandler(
+    model: IObservableMap<JSONValue>,
+    args: IObservableMap.IChangedArgs<JSONValue>
+  ): void {
+    switch (args.key) {
+      case 'kbase':
+        const cell = this as unknown as Cell;
+        updateForKBase(cell);
+        break;
+      default:
+        break;
+    }
+  }
+
 };
 
 export default factory;
